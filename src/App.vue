249d7f272e867de6ce0b9c2948a19cc8dd6ce3bb
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-06 09:39:47 -0600

    Hero completed

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,17 @@
 <template lang="pug">
 .nav
   Header
-  Button(kind="secondary") Click Me
   router-view
-  Footer
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Button from '@atoms/Button.vue';
 import Header from '@components/Header.vue';
-import Footer from '@components/Footer.vue';
 export default defineComponent({
 	components: {
 		Button,
 		Header,
-		Footer,
 	},
 });
 </script>
-
-<style lang="stylus">
-@import '~@/_tokens.styl'
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  color #2c3e50
-  background-color basePrimary
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+.home
+  .hero--container
+    .hero
+      h1 Award-winning custom designs and digital branding solutions
+      p With over 10 years in the industry, we are experienced in creating fully
+        | responsive websites, app design, and engaging brand experiences.
+        | Find out more about our services.
+      Button Learn More
+  Footer
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<style lang="stylus" scoped>
+@import '~@/_tokens.styl'
+.hero
+  background-color transparent
+  color fontColorSecondary
+  max-width 320px
+  margin 0 auto
+  height 850px
+  padding-top 75px
+  text-align center
+  & > *
+    margin-bottom 30px
+  &--container
+    background-color accent
+    background-image url("~@assets/home/desktop/image-hero-phone.png"), url("~@assets/home/desktop/bg-pattern-hero-home.svg")
+    background-repeat no-repeat
+    background-position-x center, left
+    background-position-y 300px, center
+@media screen and (min-width 768px)
+  .hero
+    max-width 700px
+    background-color accent
+    padding 50px 100px
+    border-radius borderRadius
+    background-image url("~@assets/home/desktop/image-hero-phone.png"), url("~@assets/home/desktop/bg-pattern-hero-home.svg")
+    background-repeat no-repeat
+    background-position-x center, calc(100% + 100px)
+    background-position-y 300px, center
+    &--container
+      background-image none
+      background-color transparent
+@media screen and (min-width 1200px)
+  .hero
+    max-width 1100px
+    text-align start
+    height 640px
+    padding-top 130px
+    padding-left 90px
+    padding-right 40%
+    background-position-x calc(100% + 90px), calc(100%)
+    background-position-y -30px, center
+</style>
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Footer from '@components/Footer.vue';
+import Button from '@atoms/Button.vue';
+export default defineComponent({
+	components: {
+		Footer,
+		Button,
+	},
+});
 </script>
