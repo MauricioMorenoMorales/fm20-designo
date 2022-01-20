48b51928b4d067ba299de978b4e689c65d7fc704
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-20 16:44:00 -0600

    Hero completed

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,12 +6,17 @@
 			source(media="screen and (min-width: 768px)" srcset="~@assets/about/tablet/image-about-hero.jpg")
 			img(src="~@assets/about/mobile/image-about-hero.jpg" alt="Hero illustration")
 		section.about-hero__text
-			h1 About us
+			h1 About Us
 			p Founded in 2010, we are a creative agency that produces lasting results for
 				| our clients. We’ve partnered with many startups, corporations, and nonprofits
 				| alike to craft designs that make real impact. We’re always looking forward to
 				| creating brands, products, and digital experiences that connect with our clients’
 				| audiences.
+	article.about-talent
+		picture
+			source(media="screen and (min-width: 1200px)" srcset="~@assets/about/desktop/image-world-class-talent.jpg")
+			source(media="screen and (min-width: 768px)" srcset="~@assets/about/tablet/image-world-class-talent.jpg")
+			img(src="~@assets/about/mobile/image-world-class-talent.jpg" alt="Talent illustration")
 	Footer
 </template>
 
@@ -39,9 +44,28 @@
 		margin 0 auto
 		border-radius borderRadius
 		overflow hidden
+		margin-bottom 100px
 		&__text
-			background-image url
 			padding 80px 60px
+			background-image url("~@assets/about/tablet/bg-pattern-circle-about-hero.svg")
+			background-position 0
+			background-position-y -200px
+@media screen and (min-width 1200px)
+	.about-hero
+		max-width 1100px
+		display grid
+		grid-template-columns 1fr 475px
+		grid-template-areas "text image"
+		& picture
+			grid-area image
+		&__text
+			display flex
+			flex-direction column
+			justify-content center
+			padding 0 90px
+			text-align start
+			background-position 0
+			background-position-x calc(100% + 10px)
 </style>
 
 <script lang="ts">
