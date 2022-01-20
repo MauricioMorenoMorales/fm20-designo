ff46a19b01cbfdf9d883b23539cb4850f051ac88
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-20 16:13:45 -0600

    About hero completed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,55 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+.about
+	article.about-hero
+		picture
+			source(media="screen and (min-width: 1200px)" srcset="~@assets/about/desktop/image-about-hero.jpg")
+			source(media="screen and (min-width: 768px)" srcset="~@assets/about/tablet/image-about-hero.jpg")
+			img(src="~@assets/about/mobile/image-about-hero.jpg" alt="Hero illustration")
+		section.about-hero__text
+			h1 About us
+			p Founded in 2010, we are a creative agency that produces lasting results for
+				| our clients. We’ve partnered with many startups, corporations, and nonprofits
+				| alike to craft designs that make real impact. We’re always looking forward to
+				| creating brands, products, and digital experiences that connect with our clients’
+				| audiences.
+	Footer
 </template>
+
+<style lang="stylus" scoped>
+@import '~@/_tokens.styl'
+.about-hero
+	& picture > img
+		width 100%
+	&__text
+		text-align center
+		background-image url("~@assets/about/mobile/bg-pattern-circle-about-hero.svg")
+		background-position-x 100%
+		background-position-y -120px
+		background-repeat no-repeat
+		margin-top -3px
+		padding 80px 20px
+		background-color accent
+		color fontColorSecondary
+		& h1
+			margin 0
+			margin-bottom 20px
+@media screen and (min-width 768px)
+	.about-hero
+		max-width 700px
+		margin 0 auto
+		border-radius borderRadius
+		overflow hidden
+		&__text
+			background-image url
+			padding 80px 60px
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Footer from '@components/Footer.vue';
+export default defineComponent({
+	components: {
+		Footer,
+	},
+});
+</script>
