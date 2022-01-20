9d10d4a25d1d7505d13ac271cd8889fc515a0e99
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-01-20 16:52:05 -0600

    World class talent added

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,6 +17,15 @@
 			source(media="screen and (min-width: 1200px)" srcset="~@assets/about/desktop/image-world-class-talent.jpg")
 			source(media="screen and (min-width: 768px)" srcset="~@assets/about/tablet/image-world-class-talent.jpg")
 			img(src="~@assets/about/mobile/image-world-class-talent.jpg" alt="Talent illustration")
+		section.about-talent__text
+			h2 World-class talent
+			p We are a crew of strategists, problem-solvers, and technologists.
+				|Every design is thoughtfully crafted from concept to launch, ensuring success
+				|in its given market. We are constantly updating our skills in a myriad of platforms.
+			p Our team is multi-disciplinary and we are not merely interested in form — content and
+				|meaning are just as important. We give great importance to craftsmanship, service, and
+				|prompt delivery. Clients have always been impressed with our high-quality outcomes that
+				|encapsulates their brand’s story and mission.
 	Footer
 </template>
 
@@ -38,6 +47,16 @@
 		& h1
 			margin 0
 			margin-bottom 20px
+.about-talent
+	& picture > img
+		width 100%
+	&__text
+		text-align center
+		background-color basePrimaryMoreDesaturated
+		padding 80px 20px
+		& h2
+			color accent
+			margin-bottom 20px
 @media screen and (min-width 768px)
 	.about-hero
 		max-width 700px
@@ -50,6 +69,16 @@
 			background-image url("~@assets/about/tablet/bg-pattern-circle-about-hero.svg")
 			background-position 0
 			background-position-y -200px
+	.about-talent
+		max-width 700px
+		margin 0 auto
+		border-radius borderRadius
+		overflow hidden
+		margin-bottom 100px
+		&__text
+			padding 80px 60px
+			& p
+				margin-bottom 30px
 @media screen and (min-width 1200px)
 	.about-hero
 		max-width 1100px
@@ -66,6 +95,16 @@
 			text-align start
 			background-position 0
 			background-position-x calc(100% + 10px)
+	.about-talent
+		max-width 1100px
+		display grid
+		grid-template-columns 480px 1fr
+		&__text
+			display flex
+			flex-direction column
+			justify-content center
+			padding 0 90px
+			text-align start
 </style>
 
 <script lang="ts">
